4212b42000f7885c056282ab2663aec96ae729bf
Roy Marantz <marantz5560@gmail.com>
2021-04-11 12:33:43 -0400

    fix flyspell void variable

--- lib/flycheck.el
+++ lib/flycheck.el
@@ -1,9 +1,5 @@
-;;; https://www.flycheck.org/en/latest/user/installation.html
 (use-package flycheck :straight t
   :ensure t
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode)
   )
-
-(provide 'flycheck)
-;;; flycheck.el ends here
