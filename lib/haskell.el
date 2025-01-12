92d470f3b245606573659c7693a7a78c48b05c12
Roy Marantz <marantz5560@duck.com>
2025-01-12 12:32:53 -0500

    more haskell-mode cleanup

--- lib/haskell.el
+++ lib/haskell.el
@@ -4,23 +4,22 @@
 	     :type git
 	     :host github
 	     :repo "haskell/haskell-mode")
-  :hook
-  (haskell-mode . flymake-mode)
-  (haskell-mode . flyspell-prog-mode)
   :custom
   (haskell-tags-on-save t)
   :bind (:map haskell-mode-map
-	      ("M-n" . flymake-goto-next-error)
+	      ("s-i" . haskell-navigate-imports) ; jump to imports
+	      ("M-n" . flymake-goto-next-error)	 ; use C-c-n for next-error
 	      ("M-p" . flymake-goto-prev-error)
 	      ("M-." . haskell-mode-jump-to-def-or-tag)
 	      ("C-`" . haskell-interactive-bring)
+	      ("C-c C-c" . haskell-compile)
 	      ("C-c C-l" . haskell-process-load-or-reload)
 	      ("C-c C-t" . haskell-process-do-type)
 	      ("C-c C-i" . haskell-process-do-info)
 	      )
   :config
-  (setq flymake-wrap-around t)		;flymake-goto-*-error wrap around file
   (setq haskell-process-suggest-remove-import-lines t)
   (setq haskell-process-auto-import-loaded-modules t)
   (setq haskell-process-log t)
+  (setq haskell-compile-cabal-build-command "stack build") ; instead of cabal
   )
